62543391976e439b0960d0192dff4e9a98444a0a
Coriano Harris <corianoharrispro@gmail.com>
2024-10-31 18:14:43 -0500

    Fix: node cy action issue

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
-const { defineConfig } = require("cypress");
+// cypress.config.js
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    supportFile: false,
+    video: false,
+    screenshotOnRunFailure: false
   },
-});
+})
